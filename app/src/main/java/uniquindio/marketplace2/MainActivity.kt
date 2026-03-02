796e47cad0c6af9136cf71334f70cc8e5852fea3
luisa1061278175 <luisaf.osoriog1@uqvirtual.edu.co>
2026-03-02 09:49:25 -0500

    Inicio de la aplicacion

---
+++ app/src/main/java/uniquindio/marketplace2/MainActivity.kt
@@ -0,0 +1,58 @@
+package uniquindio.marketplace2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import uniquindio.marketplace2.ui.theme.Marketplace2Theme
+
+class MainActivity : ComponentActivity() {
+    /*
+ * Esta función se llama automáticamente cuando la actividad es creada. Aquí es donde configuramos el contenido de la actividad.
+ */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Llamar al método onCreate de la superclase
+        super.onCreate(savedInstanceState)
+        // Habilitar el modo Edge-to-Edge para una mejor experiencia visual
+        enableEdgeToEdge()
+        // Configurar el contenido de la actividad utilizando Jetpack Compose
+        setContent {
+            // Aplicar el tema personalizado de la aplicación
+            Marketplace2Theme() {
+                // Se define la estructura principal de la interfaz de usuario
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Llamar a la función Greeting para mostrar el mensaje de bienvenida
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+// Función composable que muestra un mensaje de saludo
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+// Función de vista previa para el editor de Android Studio. Esta función no se ejecuta en tiempo de ejecución, solo sirve para previsualizar la UI.
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Marketplace2Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/uniquindio/marketplace2/features/home/HomeScreen.kt
@@ -0,0 +1,54 @@
+package uniquindio;
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import uniquindio.marketplace2.R
+
+@Composable
+fun HomeScreen(
+    onNavigateToLogin: () -> Unit,   // Función para navegar al Login
+    onNavigateToRegister: () -> Unit  // Función para navegar al Registro
+) {
+    // La columna principal ocupa todo el espacio y centra su contenido
+    Column(
+        modifier = Modifier.fillMaxSize(), // Ocupa todo el espacio disponible [4]
+        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically), // Espaciado y centrado vertical [4]
+        horizontalAlignment = Alignment.CenterHorizontally // Centrado horizontal [4]
+    ) {
+        // Carga la imagen desde res/drawable/welcome.png [6, 7]
+        Image(
+            painter = painterResource(id =R.drawable.logo),
+            contentDescription = "ServiMarket"
+        )
+
+        // Texto de bienvenida [8]
+        Text(text = "Pantalla de bienvenida")
+
+        // Fila horizontal para organizar los botones de acción [5, 9]
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(15.dp, Alignment.CenterHorizontally), // Espacio entre botones [4]
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = onNavigateToLogin
+            ) {
+                Text(text = "Iniciar sesión")
+            }
+
+            Button(
+                onClick = onNavigateToRegister // Lógica de navegación externa [5]
+            ) {
+                Text(text = "Crear una cuenta")
+            }
+        }
+    }
+}
